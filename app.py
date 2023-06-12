b5e009ae845aa391ac3a3cadb2e0f2a06dfccdec
Әшімбаев Арнат <ashimbayev_a@cpi.nis.edu.kz>
2023-06-12 12:35:57 +0600

    app.py added new route

--- app.py
+++ app.py
@@ -75,3 +75,5 @@ if __name__== '__main__':
     app.run()
     with app.app_context():
         db.create_all()
+
+
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 db= SQLAlchemy()
 
 class Users(db.Model):
-    #__tablename__ = "users"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True)
     email = db.Column(db.String(100), unique=True)
@@ -10,9 +10,8 @@ class Users(db.Model):
     tasks = db.relationship('Tasks', lazy=True)
 
 
-
 class Tasks(db.Model):
-   # __tablename__ = 'tasks'
+    __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     status = db.Column(db.Integer)
